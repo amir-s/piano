b482d24089aa858f5fbe3ef374c81a8fcef1375d
Amir Saboury <amir.saboury@gmail.com>
2019-10-06 12:13:24 -0400

    chord detection

--- src/components/Piano/Piano.tsx
+++ src/components/Piano/Piano.tsx
@@ -2,36 +2,45 @@ import React from 'react';
 import usePianoKeys from './usePianoKeys';
 
 export const Piano = () => {
-  const {keys, ids} = usePianoKeys();
+  const {keys, map, chord} = usePianoKeys();
 
   const allKeys = [];
-  for (let i = 0; i < 70; i++) {
-    if (i % 12 === 0 || i % 12 === 5) {
-      allKeys.push(
-        <li key={`group-${i}`}>
-          <div className={`anchor ${ids[i] ? 'active' : ''}`}>
-            <div className="indicator">⬤</div>
-          </div>
-        </li>,
-      );
-    } else {
-      allKeys.push(
-        <li key={`group-${i}`}>
-          <span className={`${ids[i] ? 'active' : ''}`}>
-            <div className="indicator">⬤</div>
-          </span>
-          <div className={`anchor ${ids[i + 1] ? 'active' : ''}`}>
-            <div className="indicator">⬤</div>
-          </div>
-        </li>,
-      );
-      i++;
+  for (let octave = 3; octave < 6; octave++) {
+    for (let i = octave * 12; i < octave * 12 + 12; i++) {
+      if (i % 12 === 0 || i % 12 === 5) {
+        allKeys.push(
+          <li key={`group-${i}`}>
+            <div className={`anchor ${map[i] ? 'active' : ''}`}>
+              <div className="indicator">⬤</div>
+            </div>
+          </li>,
+        );
+      } else {
+        allKeys.push(
+          <li key={`group-${i}`}>
+            <span className={`${map[i] ? 'active' : ''}`}>
+              <div className="indicator">⬤</div>
+            </span>
+            <div className={`anchor ${map[i + 1] ? 'active' : ''}`}>
+              <div className="indicator">⬤</div>
+            </div>
+          </li>,
+        );
+        i++;
+      }
     }
   }
   return (
     <>
       <div id="p-wrapper">
         <ul id="piano">{allKeys}</ul>
+      </div>
+      <div>
+        {chord && (
+          <h2>
+            {chord.base} {chord.type}
+          </h2>
+        )}
       </div>
     </>
   );
--- src/components/Piano/usePianoKeys.ts
+++ src/components/Piano/usePianoKeys.ts
@@ -1,4 +1,5 @@
 import {useRemoteState} from 'hooks/remote';
+import {is} from '@babel/types';
 
 const LABELS = [
   'C',
@@ -29,12 +30,64 @@ const getKeyDetail = function(keyId: number) {
   };
 };
 
+const CHORDS_CONFIG = [
+  {
+    label: 'Major',
+    steps: [4, 7],
+  },
+  {
+    label: 'Minor',
+    steps: [3, 7],
+  },
+  {
+    label: 'Dim',
+    steps: [3, 6],
+  },
+  {
+    label: 'Aug',
+    steps: [4, 8],
+  },
+  {
+    label: 'Major 7',
+    steps: [4, 7, 10],
+  },
+  {
+    label: 'Minor 7',
+    steps: [3, 7, 10],
+  },
+];
+
+const CHORDS = {};
+LABELS.forEach((base, index) => {
+  CHORDS_CONFIG.forEach(chord => {
+    const notes = chord.steps.map(i => (index + i) % 12);
+    notes.unshift(index % 12);
+    const sig = notes.sort((a, b) => a - b).join();
+    // if (CHORDS[sig]) {
+    //   console.log('Clash', base, chord.label, '<>', CHORDS[sig]);
+    // }
+    CHORDS[sig] = {
+      base,
+      type: chord.label,
+    };
+  });
+});
+console.log(CHORDS);
+const getChord = (ids: number[]) => {
+  const unique = Array.from(new Set(ids.map(i => i % 12)))
+    .sort((a, b) => a - b)
+    .join();
+  return CHORDS[unique];
+};
 export default function usePianoKeys() {
   const remoteKeys = useRemoteState<KeysState>('keys', {});
-  const ids = {};
+  const map = {};
+  const ids = [];
   const keys = Object.keys(remoteKeys).map(i => {
-    ids[~~i] = true;
+    map[~~i] = true;
+    ids.push(~~i);
     return getKeyDetail(~~i);
   });
-  return {keys, ids};
+  const chord = getChord(ids);
+  return {keys, map, ids, chord};
 }
