97026c31c769c779d7921371a194885dffc9fab2
Amir Saboury <amir.saboury@gmail.com>
2019-10-12 13:37:27 -0400

    Updates

---
+++ precache-manifest.d233b30119e0df6f078cfb53a2070ba4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "543cab9a1672b9a957f5a7d3ee178c58",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d367678699530f61e5d0",
+    "url": "/static/css/2.909d4970.chunk.css"
+  },
+  {
+    "revision": "fbb2b66fa058bedac1db",
+    "url": "/static/css/main.d02d8075.chunk.css"
+  },
+  {
+    "revision": "d367678699530f61e5d0",
+    "url": "/static/js/2.679b2424.chunk.js"
+  },
+  {
+    "revision": "fbb2b66fa058bedac1db",
+    "url": "/static/js/main.f74969e7.chunk.js"
+  },
+  {
+    "revision": "f8ffd7f8f0343f396036",
+    "url": "/static/js/runtime-main.35a1ea2c.js"
+  }
+]);
